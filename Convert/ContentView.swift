6d7eaab4c0f1b7860aaf17762ab3bdc08ef10c98
Brian Foshee <brianfoshee@gmail.com>
2020-05-02 16:43:12 -0400

    Day 19: Build a converter. Converting length.

--- Convert/ContentView.swift
+++ Convert/ContentView.swift
@@ -9,8 +9,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let units: [UnitLength] = [
+        UnitLength.feet,
+        UnitLength.yards,
+        UnitLength.miles,
+        UnitLength.meters,
+        UnitLength.kilometers,
+    ]
+    @State private var quantityFrom: String = ""
+    @State private var unitFrom: Int = 0 // default is feed
+    @State private var unitTo: Int = 3 // default is meters
+    private var quantityTo: Double {
+        let input: Double = Double(quantityFrom) ?? 0
+        let from = Measurement(value: input, unit: self.units[unitFrom])
+        let to = from.converted(to: self.units[unitTo])
+        return to.value
+    }
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                // TextField for quantity
+                TextField("Input Amount", text: $quantityFrom)
+
+                // Picker for unit from
+                Section(header: Text("Unit to convert from")) {
+                    Picker("Unit From", selection: $unitFrom) {
+                        ForEach(0 ..< self.units.count) { i in
+                            Text("\(self.units[i].symbol)")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+
+                // Picker for unit to
+                Section(header: Text("Unit to convert to")) {
+                    Picker("Unit To", selection: $unitTo) {
+                        ForEach(0 ..< self.units.count) { i in
+                            Text("\(self.units[i].symbol)")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+
+                // Text to show output
+                Section(header: Text("Output Amount")) {
+                    Text("\(quantityTo) \(self.units[unitTo].symbol)")
+                }
+            }
+            .navigationBarTitle("Convert")
+        }
     }
 }
 
