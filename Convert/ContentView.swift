26b434885366bde7537f7193114ac26fb026a80e
Brian Foshee <brianfoshee@gmail.com>
2020-05-02 16:18:18 -0400

    Initial Commit

---
+++ Convert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Convert
+//
+//  Created by Brian Foshee on 2/5/20.
+//  Copyright © 2020 Brian Foshee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
