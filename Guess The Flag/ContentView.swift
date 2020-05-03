d49d1872c615d66fc0537c372f046725415d68ba
Brian Foshee <brianfoshee@gmail.com>
2020-05-03 17:47:01 -0400

    Day 22: Add correct flag to wrong message

--- Guess The Flag/ContentView.swift
+++ Guess The Flag/ContentView.swift
@@ -64,7 +64,7 @@ struct ContentView: View {
             scoreTitle = "Correct"
             score += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
         }
 
         showingScore = true
