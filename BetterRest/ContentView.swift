8f12478812a500d26dee952ee9b979db3de44121
Brian Foshee <brianfoshee@gmail.com>
2020-05-07 19:42:37 -0400

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Brian Foshee on 7/5/20.
+//  Copyright © 2020 Brian Foshee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
