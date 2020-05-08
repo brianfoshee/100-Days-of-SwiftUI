4ab2da1386cc113aa5d130629c31679318326a90
Brian Foshee <brianfoshee@gmail.com>
2020-05-07 23:22:17 -0400

    Dat 27: Core ML Model, predicting based on the model

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,22 +9,79 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
-    @State private var wakeup: Date = Date()
+    @State private var wakeUp: Date = Date()
+    @State private var sleepAmount: Double = 8.0
+    @State private var coffeeAmount: Int = 1
+
+    @State private var alertTitle: String = ""
+    @State private var alertMessage: String = ""
+    @State private var showingAlert: Bool = false
 
     var body: some View {
-        VStack {
-            Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                Text("\(sleepAmount, specifier: "%g") hours")
-            }
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
 
-            // looks weird with text on the left
-            // two options:
-            // 1) embed in a Form
-            // 2) use hide labels
-            DatePicker("Please enter a date", selection: $wakeup, in: Date()...)
-                .labelsHidden()
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+
+                Text("Desired amount of sleep")
+                    .font(.headline)
+
+                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                    Text("\(sleepAmount, specifier: "%g") hours")
+                }
+
+                Text("Daily coffee intake")
+                    .font(.headline)
+
+                Stepper(value: $coffeeAmount, in: 1...20) {
+                    if coffeeAmount == 1 {
+                        Text("1 cup")
+                    } else {
+                        Text("\(coffeeAmount) cups")
+                    }
+                }
+            }
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing:
+                Button(action: calculateBedTime) {
+                    Text("Calculate")
+                }
+            )
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text(alertTitle),
+                      message: Text(alertMessage),
+                      dismissButton: .default(Text("OK"))
+                )
+            }
         }
+    }
+
+    func calculateBedTime() {
+        let model: SleepCalculator = SleepCalculator()
+        let components: DateComponents = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour: Int = (components.hour ?? 0) * 60 * 60
+        let minute: Int = (components.minute ?? 0) * 60
+
+        do {
+            let prediction: SleepCalculatorOutput = try model.prediction(
+                wake: Double(hour + minute),
+                estimatedSleep: sleepAmount,
+                coffee: Double(coffeeAmount)
+            )
+            let sleepTime: Date = wakeUp - prediction.actualSleep
+            let formatter: DateFormatter = DateFormatter()
+            formatter.timeStyle = .short
+
+            alertMessage = formatter.string(from: sleepTime)
+            alertTitle = "Your ideal bedtime is..."
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showingAlert = true
     }
 }
 
