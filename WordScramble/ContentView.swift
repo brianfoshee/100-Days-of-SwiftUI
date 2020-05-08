8c875116f3c35e55d4622028407b7dd7ce615b7f
Brian Foshee <brianfoshee@gmail.com>
2020-05-08 17:38:48 -0400

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Brian Foshee on 8/5/20.
+//  Copyright © 2020 Brian Foshee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
