d47a16b35689864c1606476c58c4a290ea3aa43b
Brian Foshee <brianfoshee@gmail.com>
2020-05-09 19:01:00 -0400

    Day 33: Add custom transition modifier

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -20,13 +20,38 @@ struct ContentView: View {
                 }
             }
             if isShowingRed {
-            Rectangle()
-                .fill(Color.red)
-                .frame(width: 200, height: 200)
-                .transition(.asymmetric(insertion: .scale,
-                                        removal: .opacity))
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(
+//                        .asymmetric(insertion: .scale,
+//                                    removal: .opacity)
+                        .pivot
+                )
             }
         }
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content.rotationEffect(
+            .degrees(amount),
+            anchor: anchor
+        )
+        .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
     }
 }
 
