5c52a8288c05cdda7788b27ef62e359354c62cf2
Brian Foshee <brianfoshee@gmail.com>
2020-05-09 17:08:58 -0400

    Day 33: animate gestures

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,25 +9,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled: Bool = false
     @State private var dragAmount: CGSize = CGSize.zero
 
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .offset(dragAmount)
-            .gesture(
-                DragGesture()
-                    .onChanged { drag in
-                        self.dragAmount = drag.translation
-                    }
-                    .onEnded { _ in
-                        withAnimation(.spring()) {
-                            self.dragAmount = .zero
-                        }
-
-                    }
-            )
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(self.letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(self.enabled ? Color.blue : Color.red)
+                    .offset(self.dragAmount)
+                    .animation(
+                        Animation.default.delay(Double(num) / 20)
+                )
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { drag in
+                    self.dragAmount = drag.translation
+                }
+                .onEnded { _ in
+                    self.dragAmount = .zero
+                    self.enabled.toggle()
+                }
+        )
     }
 }
 
