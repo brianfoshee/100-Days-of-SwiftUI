45e156cbb3091367a0107940a9c50f77521c8d66
Brian Foshee <brianfoshee@gmail.com>
2020-05-08 20:26:20 -0400

    Day 30: build word guess game

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -9,8 +9,115 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords: [String] = [String]()
+    @State private var rootWord: String = ""
+    @State private var newWord: String = ""
+
+    @State private var errorTitle: String = ""
+    @State private var errorMessage: String = ""
+    @State private var showingAlert: Bool = false
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .autocapitalization(.none)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                List(usedWords, id: \.self) { i in
+                    Image(systemName: "\(i.count).circle")
+                    Text(i)
+                }
+            }
+        .navigationBarTitle(rootWord)
+        }
+        .onAppear(perform: startGame)
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text(errorTitle),
+                  message: Text(errorMessage),
+                  dismissButton: .default(Text("OK")))
+        }
+    }
+
+    func addNewWord() {
+        let answer: String = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        guard answer.count > 0 else {
+            return
+        }
+
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not possible", message: "That isn't a real word.")
+            return
+        }
+
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+
+    func startGame() {
+        // fine the URL for start.txt
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // load into string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // split into array, on newline
+                let allWords = startWords.components(separatedBy: "\n")
+
+                // pick a random word
+                rootWord = allWords.randomElement() ?? "silkworm"
+
+                return
+            }
+        }
+
+        fatalError("Could not load start.txt from bundle")
+    }
+
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+
+    func isPossible(word: String) -> Bool {
+        var tempWord: String = rootWord
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    func isReal(word: String) -> Bool {
+        let checker: UITextChecker = UITextChecker()
+        let range: NSRange = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange: NSRange = checker.rangeOfMisspelledWord(in: word,
+                                                            range: range,
+                                                            startingAt: 0,
+                                                            wrap: false,
+                                                            language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingAlert = true
     }
 }
 
