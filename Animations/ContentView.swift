381e923f76bcb17f125adeaf30849d7ac7c8a7c0
Brian Foshee <brianfoshee@gmail.com>
2020-05-09 15:29:44 -0400

    Day 32: Rotation 3D Effects

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,58 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount: CGFloat = 1
+    @State private var animationAmount: Double = 0
     var body: some View {
-        print(animationAmount)
-
-        return VStack {
-            Button("Tap me") {
-                // self.animationAmount += 1
-            }
-            .padding(50)
-            .background(Color.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-                // .scaleEffect(animationAmount)
-                //.animation(.interpolatingSpring(stiffness: 50, damping: 1))
-                //.animation(.easeInOut(duration: 2))
-                //.blur(radius: (animationAmount - 1) * 3)
-                .overlay(
-                    Circle()
-                        .stroke(Color.red)
-                        .scaleEffect(animationAmount)
-                        .opacity(Double(2 - animationAmount))
-                        .animation(
-                            Animation.easeInOut(duration: 2)
-                                .repeatForever(autoreverses: true)
-                    )
-            )
-                .onAppear {
-                    self.animationAmount = 2
-            }
-
-            Stepper("Scale amount", value: $animationAmount.animation(
-                Animation.easeInOut(duration: 1)
-                    .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-
-            VStack {
-                Stepper("Scale amount",
-                        value: $animationAmount.animation(),
-                        in: 1...10)
-
-                Spacer()
-
-                Button("Tap me") {
-                    self.animationAmount += 1
-                }
-                .padding(40)
-                .background(Color.red)
-                .foregroundColor(.white)
-                .clipShape(Circle())
-                .scaleEffect(animationAmount)
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(
+                stiffness: 5, damping: 1)
+            ) {
+                self.animationAmount += 360
             }
         }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(
+            .degrees(animationAmount),
+            axis: (x: 0, y: 1, z: 0)
+        )
     }
 }
 
