726d5b9cef888ed13581e3556a6be84af47783f1
Brian Foshee <brianfoshee@gmail.com>
2020-05-09 17:44:24 -0400

    Day 33: showing and hiding views

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,33 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    let letters = Array("Hello SwiftUI")
-    @State private var enabled: Bool = false
-    @State private var dragAmount: CGSize = CGSize.zero
+    @State private var isShowingRed: Bool = false
+
 
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count) { num in
-                Text(String(self.letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(self.enabled ? Color.blue : Color.red)
-                    .offset(self.dragAmount)
-                    .animation(
-                        Animation.default.delay(Double(num) / 20)
-                )
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+            if isShowingRed {
+            Rectangle()
+                .fill(Color.red)
+                .frame(width: 200, height: 200)
+                .transition(.asymmetric(insertion: .scale,
+                                        removal: .opacity))
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged { drag in
-                    self.dragAmount = drag.translation
-                }
-                .onEnded { _ in
-                    self.dragAmount = .zero
-                    self.enabled.toggle()
-                }
-        )
     }
 }
 
