920f6a8a7a989592d4a9d4c633f312eadbb163fb
Brian Foshee <brianfoshee@gmail.com>
2020-05-09 16:35:53 -0400

    Day 32: ANimating gestures

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,18 +9,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount: Double = 0
-    @State private var enabled: Bool = false
+    @State private var dragAmount: CGSize = CGSize.zero
+
     var body: some View {
-        Button("Tap Me") {
-            self.enabled.toggle()
-        }
-        .frame(width: 200, height: 200)
-        .background(enabled ? Color.blue : Color.red)
-        .animation(.default)
-        .foregroundColor(.white)
-        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { drag in
+                        self.dragAmount = drag.translation
+                    }
+                    .onEnded { _ in
+                        withAnimation(.spring()) {
+                            self.dragAmount = .zero
+                        }
+
+                    }
+            )
     }
 }
 
