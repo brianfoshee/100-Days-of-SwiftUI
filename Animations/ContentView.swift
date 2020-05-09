6faf1e3a9ae7f28718d827b3569be15fd1a32f70
Brian Foshee <brianfoshee@gmail.com>
2020-05-09 14:28:31 -0400

    Day 32: Animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,8 +9,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount: CGFloat = 1
     var body: some View {
-        Text("Hello, World!")
+        print(animationAmount)
+
+        return VStack {
+            Button("Tap me") {
+                // self.animationAmount += 1
+            }
+            .padding(50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+                // .scaleEffect(animationAmount)
+                //.animation(.interpolatingSpring(stiffness: 50, damping: 1))
+                //.animation(.easeInOut(duration: 2))
+                //.blur(radius: (animationAmount - 1) * 3)
+                .overlay(
+                    Circle()
+                        .stroke(Color.red)
+                        .scaleEffect(animationAmount)
+                        .opacity(Double(2 - animationAmount))
+                        .animation(
+                            Animation.easeInOut(duration: 2)
+                                .repeatForever(autoreverses: true)
+                    )
+            )
+                .onAppear {
+                    self.animationAmount = 2
+            }
+
+            Stepper("Scale amount", value: $animationAmount.animation(
+                Animation.easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+
+            VStack {
+                Stepper("Scale amount",
+                        value: $animationAmount.animation(),
+                        in: 1...10)
+
+                Spacer()
+
+                Button("Tap me") {
+                    self.animationAmount += 1
+                }
+                .padding(40)
+                .background(Color.red)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .scaleEffect(animationAmount)
+            }
+        }
     }
 }
 
