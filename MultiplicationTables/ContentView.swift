ef44cf72c8386db42734d5dd2f0f2c4b2f8c1de8
Brian Foshee <brianfoshee@gmail.com>
2020-05-10 17:05:28 -0400

    Day 32: Game almost done

--- MultiplicationTables/ContentView.swift
+++ MultiplicationTables/ContentView.swift
@@ -14,35 +14,31 @@ You should randomly generate as many questions as they asked for, within the dif
 
 import SwiftUI
 
+struct Question {
+    let x: Int
+    let y: Int
+    var answer: Int {
+        return x * y
+    }
+
+    func isCorrect(guess num: Int) -> Bool {
+        return answer == num
+    }
+}
+
 struct ContentView: View {
     @State private var showingConfig: Bool = true
-    @State private var tableSelection: Int = 1
+    @State private var tableSelection: Int = 4
     @State private var questionsSelection: Int = 0
+    @State private var currentQuestionIndex: Int = 0
+    @State private var guess: String = ""
+    @State private var multiplicationTable: [Question] = [Question]()
+
+    @State private var isShowingAlert: Bool = false
+    @State private var alertTitle: String = ""
+    @State private var alertMessage: String = ""
 
     var questionAmount: [Int] = [5, 10, 20, 24]
-    var multiplicationTable: [[Int]:Int] {
-        // generate the full multiplication table, 24 combinations
-        // If table is 5, the dictionary looks like:
-        // [5, 1]: 5, [1, 5]: 5, [5, 2]: 10, [2, 5]: 10 etc
-        var fullTable: [[Int]:Int] = [[Int]:Int]()
-        for i in 1...12 {
-            fullTable[[i, tableSelection]] = i * tableSelection
-            fullTable[[tableSelection, i]] = i * tableSelection
-        }
-
-        if questionsSelection == 3 {
-            return fullTable
-        }
-
-        // Select a random assortment of questions
-        var output: [[Int]:Int] = [[Int]:Int]()
-        for _ in 0..<questionAmount[questionsSelection] {
-            if let e = fullTable.randomElement() {
-                output[e.key] = e.value
-            }
-        }
-        return output
-    }
 
     var body: some View {
         NavigationView {
@@ -69,20 +65,76 @@ struct ContentView: View {
                 .navigationBarItems(trailing:
                     Button("Play") {
                         self.showingConfig = false
+                        // start game
+                        self.generateTable()
+                        self.currentQuestionIndex = 0
                     }
                 )
             } else {
-                HStack{
-                    Text("doing \(tableSelection) multiplication tables")
+                Form {
+                    Text("\(multiplicationTable[currentQuestionIndex].x) X \(multiplicationTable[currentQuestionIndex].y) = ")
+
+                    TextField("answer:", text: $guess, onCommit: checkGuess)
                 }
-                .navigationBarTitle("Multiplication Tables")
+                .navigationBarTitle("\(tableSelection) Times Tables")
                 .navigationBarItems(leading: Button("Config"){
                     self.showingConfig = true
                     }, trailing: Button("Start Over") {
-                        // TODO start over
+                        // start over
+                        self.generateTable()
+                        self.currentQuestionIndex = 0
                 })
+                .alert(isPresented: $isShowingAlert) {
+                    Alert(title: Text(alertTitle),
+                          message: Text(alertMessage),
+                          dismissButton: .default(Text("OK")){
+                            self.guess = ""
+                            if self.currentQuestionIndex == self.multiplicationTable.count - 1 {
+                                // done, show score
+                            } else {
+                                self.currentQuestionIndex += 1
+                            }
+                        })
+                }
             }
         }
+    }
+
+    func checkGuess() {
+        let question: Question = multiplicationTable[currentQuestionIndex]
+        if let guess = Int(guess) {
+            let correct: Bool = question.isCorrect(guess: guess)
+            if correct {
+                // score plus 1
+                alertTitle = "Correct!"
+                alertMessage = ""
+            } else {
+                alertTitle = "Incorrect"
+                alertMessage = "\(question.x) x \(question.y) = \(question.answer)"
+            }
+            isShowingAlert = true
+        }
+    }
+
+    func generateTable() {
+        // generate the full multiplication table, 24 combinations
+        var fullTable: [Question] = [Question]()
+        for i in 1...12 {
+            fullTable.insert(Question(x: i, y: tableSelection), at: 0)
+            fullTable.insert(Question(x: tableSelection, y: i), at: 0)
+        }
+        fullTable.shuffle()
+
+        if questionsSelection == 3 {
+            self.multiplicationTable = fullTable
+        }
+
+        // Select a random assortment of questions
+        var output: [Question] = [Question]()
+        for i in 0 ..< questionAmount[questionsSelection] {
+            output.insert(fullTable[i], at: 0)
+        }
+        self.multiplicationTable = output
     }
 }
 
