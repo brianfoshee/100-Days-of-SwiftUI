3bfdba680a702618742e07451bef84edabd76910
Brian Foshee <brianfoshee@gmail.com>
2020-05-16 14:11:59 -0400

    Day 37: Add items to a class

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -9,15 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount: Int = UserDefaults.standard.integer(forKey: "Tap")
-
+    @ObservedObject var expenses: Expenses = Expenses()
     var body: some View {
-        Button("tap count: \(tapCount)") {
-            self.tapCount += 1
-            UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 100)
+                    self.expenses.items.append(expense)
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
         }
-
     }
+
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+class Expenses: ObservableObject {
+    @Published var items: [ExpenseItem] = [ExpenseItem]()
+}
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
 }
 
 struct ContentView_Previews: PreviewProvider {
