ccc6e7ddf873719844f4fcceef72a9aa5764703e
Brian Foshee <brianfoshee@gmail.com>
2020-05-17 14:57:01 -0400

    Day 40: Building UI. Image assets, date formatters in JSON parsing.

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,14 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+
     var body: some View {
-        VStack {
-            GeometryReader { geo in
-                Image("moab")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: geo.size.width)
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("Detail view")) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
             }
+            .navigationBarTitle("Moonshot")
         }
     }
 }
