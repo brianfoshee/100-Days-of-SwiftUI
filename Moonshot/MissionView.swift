90ec3cccda763a9ba41e6e2b8f486f18c5ed31ba
Brian Foshee <brianfoshee@gmail.com>
2020-05-17 15:43:12 -0400

    Day 41: Add a MissionView

---
+++ Moonshot/MissionView.swift
@@ -0,0 +1,40 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Brian Foshee on 17/5/20.
+//  Copyright © 2020 Brian Foshee. All rights reserved.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geo.size.width * 0.7)
+                        .padding(.top)
+
+                    Text(self.mission.description)
+                        .padding()
+
+                    Spacer(minLength: 25)
+                }
+            }
+        }
+        .navigationBarTitle(Text(mission.displayName), displayMode: .inline)
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static var previews: some View {
+        MissionView(mission: missions[0])
+    }
+}
