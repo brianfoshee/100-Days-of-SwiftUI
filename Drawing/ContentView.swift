54bd05cd4c24610c370df9e20792e410fb17b860
Brian Foshee <brianfoshee@gmail.com>
2020-05-21 21:36:39 -0400

    Day 43: Drawing arcs

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -21,12 +21,38 @@ struct Triangle: Shape {
     }
 }
 
+struct Arc: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+
+    func path(in rect: CGRect) -> Path {
+        // 0 degrees is to the right. this adjusts so 0 is up top.
+        let angleAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - angleAdjustment
+        let modifiedEnd = endAngle - angleAdjustment
+
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY),
+                    radius: rect.width / 2,
+                    startAngle: modifiedStart,
+                    endAngle: modifiedEnd,
+                    clockwise: clockwise)
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Triangle()
-            .fill(Color.red)
-            .frame(width: 300, height: 300)
+        VStack {
+            Triangle()
+                .fill(Color.red)
+                .frame(width: 200, height: 200)
 
+            Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+                .stroke(Color.blue, lineWidth: 10)
+                .frame(width: 200, height: 120)
+        }
     }
 }
 
