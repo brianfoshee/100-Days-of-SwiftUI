5f9e12b43b691b367ce51156dd5297ff501b8859
Brian Foshee <brianfoshee@gmail.com>
2020-05-21 21:27:38 -0400

    Day 43: Using Shape protocol for reusable shapes

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,15 +8,24 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
-        }
-        .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        Triangle()
+            .fill(Color.red)
+            .frame(width: 300, height: 300)
 
     }
 }
