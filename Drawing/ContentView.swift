7f2b0361884f31e9c93cc46890397dc88873c0fa
Brian Foshee <brianfoshee@gmail.com>
2020-05-23 15:34:51 -0400

    Day 44: Metal .drawingGroup

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -90,15 +90,47 @@ struct Flower: Shape {
     }
 }
 
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 struct ContentView: View {
     @State private var petalOffset: Double = -20
     @State private var petalWidth: Double = 100
 
+    @State private var colorCycle = 0.0
+
     var body: some View {
+/*
         VStack {
             Flower(petalOffset: petalOffset, petalWidth: petalWidth)
                 //.stroke(Color.red, lineWidth: 1)
-                .fill(Color.red, style: FillStyle(eoFill: true))
+                .fill(Color.blue, style: FillStyle(eoFill: true))
 
             Text("Offset")
             Slider(value: $petalOffset, in: -40...40)
@@ -108,6 +140,15 @@ struct ContentView: View {
             Slider(value: $petalWidth, in: 0...100)
                 .padding(.horizontal)
         }
+ */
+
+        VStack {
+            ColorCyclingCircle(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
+
+            Slider(value: $colorCycle)
+        }
+
 
     }
 }
