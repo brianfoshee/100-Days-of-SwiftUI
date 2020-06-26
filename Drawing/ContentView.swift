656c67989d9d93f350076d4cba6d6eaadf993f06
Brian Foshee <brianfoshee@gmail.com>
2020-06-26 13:43:19 -0400

    Day 45: Blending

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -120,6 +120,46 @@ struct ColorCyclingCircle: View {
 }
 
 struct ContentView: View {
+    @State private var amount: CGFloat = 1.0
+
+    var body: some View {
+        VStack {
+            ZStack {
+                Circle()
+                    .fill(Color(red: 1, green: 0, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+                    .saturation(Double(amount))
+                    .blur(radius: (1 - amount) * 20)
+
+                Circle()
+                    .fill(Color(red: 0, green: 1, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+                    .saturation(Double(amount))
+                    .blur(radius: (1 - amount) * 20)
+
+                Circle()
+                    .fill(Color(red: 0, green: 0, blue: 1))
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+                    .saturation(Double(amount))
+                    .blur(radius: (1 - amount) * 20)
+            }
+            .frame(width: 300, height: 300)
+
+            Slider(value: $amount)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+/*
+struct ContentView: View {
     @State private var petalOffset: Double = -20
     @State private var petalWidth: Double = 100
 
@@ -152,6 +192,8 @@ struct ContentView: View {
 
     }
 }
+
+ */
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
