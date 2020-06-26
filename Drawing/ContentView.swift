b9c0043bbadd3a60fd7426a4c549acf4110e178c
Brian Foshee <brianfoshee@gmail.com>
2020-06-26 18:52:46 -0400

    Day 45: AnimatablePair

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -28,6 +28,61 @@ struct Trapezoid: Shape {
     }
 }
 
+struct CheckerBoard: Shape {
+    var rows: Int
+    var columns: Int
+
+    public var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+
+        set {
+            self.rows = Int(newValue.first)
+            self.columns = Int(newValue.second)
+        }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored, add a rectangle
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+
+        return path
+    }
+}
+
+struct ContentView: View {
+    @State private var rows = 4
+    @State private var columns = 4
+
+    var body: some View {
+        CheckerBoard(rows: rows, columns: columns)
+        .onTapGesture {
+            withAnimation(.linear(duration: 3)) {
+                self.rows = 8
+                self.columns = 16
+            }
+        }
+
+    }
+}
+
+/*
 struct ContentView: View {
     @State private var insetAmount: CGFloat = 50
 
@@ -41,6 +96,7 @@ struct ContentView: View {
             }
     }
 }
+*/
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
