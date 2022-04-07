dd5eca78634c8559b13743c7832daac4e6150b09
Brian Foshee <brianfoshee@gmail.com>
2022-04-07 19:05:47 -0400

    Day 6 Playground

--- Day 6.playground/Contents.swift
+++ Day 6.playground/Contents.swift
@@ -1,3 +1,62 @@
 // https://www.hackingwithswift.com/100/swiftui/6
 import Cocoa
 
+let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+for os in platforms {
+    print("swift works great on \(os)")
+}
+
+// 1...12 is 1 to 12 inclusive
+for i in 1...12 {
+    print("5 x \(i) is \(5 * i)")
+}
+
+// 1..<5 is 1 to 4 (exluding the last number)
+for i in 1..<5 {
+    print("\(i)")
+}
+
+// if you don't want to use the loop var replace with _
+for _ in 1...5 {
+ print("hi")
+}
+
+// while
+
+var countdown = 10
+while countdown > 0 {
+    print("\(countdown)")
+    countdown -= 1
+}
+
+// random number generator
+let id = Int.random(in: 1...1000)
+let amount = Double.random(in: 0...1)
+
+var roll = 0
+
+while roll != 20 {
+    roll = Int.random(in: 1...20)
+    print("I rolled a \(roll)")
+}
+
+print("something about d and d")
+
+// continue keeps looping
+// break stops
+
+// fizz buzz
+for i in 1...100 {
+    let three = i.isMultiple(of: 3)
+    let five = i.isMultiple(of: 5)
+    if three && five {
+        print("Fizz Buzz")
+    } else if three {
+        print("Fizz")
+    } else if five {
+        print("Buzz")
+    } else {
+        print("\(i)")
+    }
+}
--- README.md
+++ README.md
@@ -3,9 +3,17 @@ Goal: be done by memorial day. Which is 56 days total.
 [Glossary of Swift Terms](https://www.hackingwithswift.com/glossary)
 [SwiftUI By Example](https://www.hackingwithswift.com/quick-start/swiftui)
 
+# Day 6
+7 April
+https://www.hackingwithswift.com/100/swiftui/6
+
+Loops
+
 # Day 5
 7 April
 https://www.hackingwithswift.com/100/swiftui/5
+
+Conditionals
 
 # Day 4
 6 April
