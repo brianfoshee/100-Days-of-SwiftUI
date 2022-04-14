adf3b0d87be561979efdc122016f1c8b60225af1
Brian Foshee <brianfoshee@gmail.com>
2022-04-13 21:40:27 -0400

    Day 20: gradients

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -11,8 +11,22 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             VStack(spacing: 0) {
-                Color.red
-                Color.blue
+                // https://www.hackingwithswift.com/books/ios-swiftui/gradients
+
+                // LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
+
+                // begin after 45%, end at 55%
+                /*
+                LinearGradient(gradient: Gradient(stops: [
+                       Gradient.Stop(color: .white, location: 0.45),
+                       Gradient.Stop(color: .black, location: 0.55),
+                   ]), startPoint: .top, endPoint: .bottom)
+                 */
+
+                // RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+
+                AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+
             }
 
             Text("Your Content")
