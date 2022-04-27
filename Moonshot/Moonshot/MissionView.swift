c604538a84cfca66271b3398881cc295f2858782
Brian Foshee <brianfoshee@gmail.com>
2022-04-27 18:13:02 -0400

    Day 41: add crew to mission page

--- Moonshot/Moonshot/MissionView.swift
+++ Moonshot/Moonshot/MissionView.swift
@@ -8,7 +8,26 @@
 import SwiftUI
 
 struct MissionView: View {
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+
     let mission: Mission
+    let crew: [CrewMember]
+
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("mission \(member.name)") // this shouldn't happen so fail hard if it does
+            }
+        }
+    }
+
     var body: some View {
         GeometryReader { geometry in
             ScrollView {
@@ -20,13 +39,55 @@ struct MissionView: View {
                         .padding(.top)
 
                     VStack(alignment: .leading) {
+                        // Divider exists but it's not customizable
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundColor(.lightBackground)
+                            .padding(.vertical)
+
                         Text("Mission Highlights")
                             .font(.title.bold())
                             .padding(.bottom, 5)
 
                         Text(mission.description)
+
                     }
                     .padding(.horizontal)
+                    
+                        Text("Crew")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(crew, id: \.role) { crewMember in
+                                NavigationLink {
+                                    Text("astronaut details")
+                                } label: {
+                                    HStack {
+                                        Image(crewMember.astronaut.id)
+                                            .resizable()
+                                            .frame(width: 104, height: 72)
+                                            .clipShape(Capsule())
+                                            .overlay(
+                                                Capsule()
+                                                    .strokeBorder(.white, lineWidth: 1)
+                                            )
+
+                                        VStack(alignment: .leading) {
+                                            Text(crewMember.astronaut.name)
+                                                .foregroundColor(.white)
+                                                .font(.headline)
+
+                                            Text(crewMember.role)
+                                                .foregroundColor(.secondary)
+                                        }
+                                    }
+                                    .padding(.horizontal)
+                                }
+                            }
+                        }
+                    }
                 }
                 .padding(.bottom)
             }
@@ -39,8 +100,10 @@ struct MissionView: View {
 
 struct MissionView_Previews: PreviewProvider {
     static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
     static var previews: some View {
-        MissionView(mission: missions[0])
+        MissionView(mission: missions[0], astronauts: astronauts)
             .preferredColorScheme(.dark) // this comes in from ContentView normally but during preview needs set
     }
 }
